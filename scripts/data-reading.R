11437dccf1de2be0b0fbcc8af8bfb033d782cee6
Shreyas Ragavan <sr@eml.cc>
2021-09-25 11:19:06 -0700

    Interim updates with pivot - stuck on str replace

--- scripts/data-reading.R
+++ scripts/data-reading.R
@@ -11,7 +11,8 @@ raw_data_loc <- here("data", "00_raw_data")
 wrangled_data_loc <- here("data", "01_wrangled_data")
 scripts_loc <- here("scripts")
 
-## Data file locations for convenience
+
+## Data file locations for convenience -----
 data_dictionary_file <- here(raw_data_loc, "Data_dictionary_with_description_20201105_CORRECTED.xlsx")
 raw_survey_data_file <- here(raw_data_loc, "SPEAKI_Survey_Data_deidentified.xlsx")
 
@@ -78,32 +79,86 @@ us_state_tbl <- read.xlsx(data_dictionary_file,
   unique()
 
 ## Data dictionary read in. ----
-## This needs to be processed further
+
+## This needs to be processed further We need to use the filled option
+## to unmerge and repeat the content in column.descriptions We also need
+## to make all the column_names lower case to match the header columns
+## in raw survey data
+
 data_dictionary_raw_tbl <- read.xlsx(data_dictionary_file, 
-                          sheet = "Data_dictionary") %>%
+                                     sheet = "Data_dictionary",
+                                     fillMergedCells = T) %>%
   as_tibble() %>%
   select(Column.Name, Column.Description, Code.List) %>%
-  janitor::clean_names()
+  janitor::clean_names() %>%
+  mutate(column_name = str_to_lower(column_name))
 
   
-## Reading in a sample of the survey data for testing
+## Reading in a sample of the survey data for testing -----
 ## 100 rows and all the columns 
 raw_survey_data <- read.xlsx(raw_survey_data_file,
                              rows = c(1:100)) %>%
   as_tibble()
 
 
-# Transposing the survey data to enable filtering the features and
-# substituting values from the dictionary
+# Transposing Survey data table -----
+## Transposing the survey data to enable filtering the features 
+## substituting values from the dictionary
+       
 
-raw_survey_data %>%
-select(responseid, no_change_behaviour, personal_hygiene) %>%
-pivot_longer(cols = c('no_change_behaviour', 'personal_hygiene'),
-             names_to = "key") %>% 
-pivot_wider(
-  names_from = responseid,
-  values_from = value)
+transposed_survey_data_tbl <- raw_survey_data %>%
+  select(responseid,
+         no_change_behaviour,
+         hand_wash_regularly,
+         practice_phys_distancing,
+         stay_home_sick,          
+         avoid_gathering,         
+         personal_hygiene,        
+         working_home2) %>% 
+  pivot_longer(cols = no_change_behaviour:working_home2,
+               names_to = "key") %>% 
+  pivot_wider(
+    names_from = responseid,
+    values_from = value)
 
-## TEsting the replacement of values from the dictionaries
-## TODO Create a consolidated data dictionary for looking up values?    
-## TODO Do we need to remove spurious values from the dictionary ?
+# Left JOin data dictionary to transposed survey table  ----
+## Join the data dictionary to obtain column description and code list 
+## Mutate the survey data to character to enable subsequent string
+## replacement
+
+joined_data_dict_transposed_tbl <-
+  transposed_survey_data_tbl %>%
+  left_join(data_dictionary_raw_tbl, by = c("key" = "column_name")) %>%
+  select(key, column_description, code_list, everything()) %>%
+  mutate(code_list = str_c(code_list, ";")) %>% 
+  mutate(across(starts_with("R_"), ~ as.character(.x)))
+
+
+########################################################## NEEED HELP !!
+
+## Replacing survey data table with data dictionary values 
+## TODO fix this 
+
+
+## Is code list not being obtained properly? Do we need purrr::map ?
+
+replaced_transposed_tbl <- joined_data_dict_transposed_tbl %>%
+  mutate(across(starts_with("R_"), ~ str_match(code_list,
+                                                 paste0("(", cur_column(), "=[a-zA-z]*;)"))[,2]))
+
+
+
+######################################### testing with simplified table ------
+## This works as a basic example
+## Sample string matching 
+
+sample <- joined_data_dict_transposed_tbl %>%
+  select(code_list) %>%
+  mutate(test0 = as.character(0),
+         test1 = as.character(1)) %>%
+  mutate(test_parsed_0 = str_match(code_list, paste0(test0, "=([a-zA-Z]*);"))[,2]) %>% 
+  mutate(test_parsed_1 = str_match(code_list, paste0(test1, "=([a-zA-Z]*);"))[,2])
+
+
+
+
